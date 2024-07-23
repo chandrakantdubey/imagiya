32023d962cc6f4f9a8e41d7d0300e659334dbc3b
chandrakantdubey <919chandrakant@gmail.com>
2024-07-23 23:32:06 +0530

    implement clerk web hooks and included form actions

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,10 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 // export default clerkMiddleware();
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-
-// import { authMiddleware } from "@clerk/nextjs";
- 
-// export default authMiddleware({
-//   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-// });
- 
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
